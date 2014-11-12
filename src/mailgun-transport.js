2e76333e581ffdbe702402c2ac4d17143ba3999c
Gilad Beeri <giladb.dev@gmail.com>
2014-11-12 16:46:26 +0200

    simplified code, support more options, like html

--- src/mailgun-transport.js
+++ src/mailgun-transport.js
@@ -3,7 +3,7 @@
 var Mailgun = require('mailgun-js');
 var packageData = require('../package.json');
 
-module.exports = function(options) {
+module.exports = function (options) {
   return new MailgunTransport(options);
 };
 
@@ -11,31 +11,14 @@ function MailgunTransport(options) {
   this.options = options || {};
   this.name = 'Mailgun';
   this.version = packageData.version;
-}
 
-MailgunTransport.prototype.send = function (mail, callback) {
-
-  var mailgun = Mailgun({
+  this.mailgun = Mailgun({
     apiKey: this.options.auth.api_key,
     domain: this.options.auth.domain || ''
   });
+}
 
-  var data = {   
-    from: mail.data.from,
-    to: mail.data.to,
-    subject: mail.data.subject,
-    text: mail.data.text
-  }
-
-  mailgun.messages().send(data,
-    function (err, body) {
-      if (err) {
-        console.log(err);
-        return callback(err);
-      }
-      return callback();
-    }
-  );
-
+MailgunTransport.prototype.send = function send(mail, callback) {
+  this.mailgun.messages().send(mail.data, callback);
 };
 
