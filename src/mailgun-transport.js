ae79196d70c432a62ae3c1f41a1bb64987f8f528
tubagus anwar <anwartubagus@gmail.com>
2020-07-07 23:18:34 +0700

    refine variable and change new Buffer to Buffer.from

--- src/mailgun-transport.js
+++ src/mailgun-transport.js
@@ -1,15 +1,15 @@
-'use strict';
+'use strict'
 
-var Mailgun = require('mailgun-js');
-var cons = require('consolidate');
-var packageData = require('../package.json');
-var series = require('async-series');
-var pickBy = require('lodash.pickby');
-var some = require('lodash.some');
-var startsWith = require('lodash.startswith');
-var request = require('request');
+const Mailgun = require('mailgun-js')
+const cons = require('consolidate')
+const packageData = require('../package.json')
+const series = require('async-series')
+const pickBy = require('lodash.pickby')
+const some = require('lodash.some')
+const startsWith = require('lodash.startswith')
+const request = require('request')
 
-var whitelistExact = [
+const whitelistExact = [
   'from',
   'to',
   'h:Reply-To',
@@ -31,107 +31,108 @@ var whitelistExact = [
   'o:tracking-opens',
   'o:require-tls',
   'o:skip-verification'
-];
-var whitelistPrefix = [
+]
+const whitelistPrefix = [
   'h:',
   'v:'
-];
+]
 
 module.exports = function (options) {
-  return new MailgunTransport(options);
-};
+  return new MailgunTransport(options)
+}
 
-function MailgunTransport(options) {
-  this.options = options || {};
-  this.name = 'Mailgun';
-  this.version = packageData.version;
-
+function MailgunTransport (options) {
+  this.options = options || {}
+  this.name = 'Mailgun'
+  this.version = packageData.version
   this.mailgun = Mailgun({
     apiKey: this.options.auth.api_key,
     domain: this.options.auth.domain || ''
-  });
-  this.messages = this.mailgun.messages();
+  })
+  this.messages = this.mailgun.messages()
 }
 
-MailgunTransport.prototype.send = function send(mail, callback) {
-  let isURL = (str) => {
-    var regex = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/;
-    if(!regex .test(str)) {
-      return false;
+MailgunTransport.prototype.send = function send (mail, callback) {
+  const isURL = (str) => {
+    /* eslint-disable */
+    const regex = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/
+    if (!regex.test(str)) {
+      return false
     } else {
-      return true;
+      return true
     }
   }
 
-  var self = this;
-  var mailData = mail.data;
+  const self = this
+  const mailData = mail.data
   series([
     function (done) {
       if (mailData.template && mailData.template.name && mailData.template.engine) {
-        mailData.template.context = mailData.template.context || {};
+        mailData.template.context = mailData.template.context || {}
         cons[mailData.template.engine](mailData.template.name, mailData.template.context, function (err, html) {
-          if (err) throw err;
-          mailData.html = html;
-          done();
-        });
+          if (err) throw err
+          mailData.html = html
+          done()
+        })
       } else {
-        done();
+        done()
       }
     },
-     function(done){
-      //convert address objects or array of objects to strings if present
-      var targets =['from','to','cc','bcc','replyTo'];
-      var count =0;
-      for (var target of targets){
-        if(target === 'replyTo'){
+    function (done) {
+      // convert address objects or array of objects to strings if present
+      const targets = ['from', 'to', 'cc', 'bcc', 'replyTo']
+      for (let target of targets) {
+        if (target === 'replyTo') {
           const newTarget = 'h:Reply-To'
-          mailData[newTarget] =mailData[target];
+          mailData[newTarget] = mailData[target]
           delete mailData[target]
           target = newTarget
-        } 
-        var addrsData = mailData[target];
-        if(addrsData !== null && (typeof addrsData === 'object' || Array.isArray(addrsData))){
-          var addrs= [];
-          var addresses = typeof addrsData === 'object' ? [addrsData] : addrsData;
-          for (var addr of addresses ){
-                if (Array.isArray(addr)){
-                  for (var add of addr){
-                    if(typeof add === 'object' && add.address){
-                      var final = add.name ? add.name + ' <' + add.address + '>' : add.address
-                      addrs.push(final);
-                    } else if (typeof add === 'string') {
-                      addrs.push(add)
-                    }
-                  }
-                } else{
-                  if(addr.address){
-                    var final = addr.name ? addr.name + ' <' + addr.address + '>' : addr.address
-                    addrs.push(final);
-                  }
-                }
-          }
-          mailData[target] = addrs.join();
         }
-        count++;
-        count == 5 ? done():null;
+        const addressData = mailData[target]
+        if (addressData !== null && (typeof addressData === 'object' || Array.isArray(addressData))) {
+          const addrs = []
+          const addresses = typeof addressData === 'object' ? [addressData] : addressData
+          for (let addr of addresses) {
+            if (Array.isArray(addr)) {
+              for (let add of addr) {
+                if (typeof add === 'object' && add.address) {
+                  const addr = add.name ? add.name + ' <' + add.address + '>' : add.address
+                  addrs.push(addr)
+                } else if (typeof add === 'string') {
+                  addrs.push(add)
+                }
+              }
+            } else {
+              if (addr.address) {
+                const addr = addr.name ? addr.name + ' <' + addr.a + '>' : addr.address
+                addrs.push(addr)
+              }
+            }
+          }
+          mailData[target] = addrs.join()
+        }
       }
+      done()
     },
     async function (done) {
       // convert nodemailer attachments to mailgun-js attachements
       if (mailData.attachments) {
-        var attachment, mailgunAttachment, data, attachmentList = [], inlineList = [];
-        for (var i in mailData.attachments) {
-          attachment = mailData.attachments[i];
+        let attachment
+        let mailgunAttachment
+        let data
+        const attachmentList = []
+        const inlineList = []
+        for (let i in mailData.attachments) {
+          attachment = mailData.attachments[i]
           // mailgunjs does not encode content string to a buffer
-          let remoteFile = attachment.path || attachment.href || attachment.full_path || undefined
-          if (remoteFile  && isURL(remoteFile)) {
-            mailgunAttachment = await request(remoteFile);
-          }
-          else {
+          const remoteFile = attachment.path || attachment.href || attachment.full_path || undefined
+          if (remoteFile && isURL(remoteFile)) {
+            mailgunAttachment = await request(remoteFile)
+          } else {
             if (typeof attachment.content === 'string') {
-              data = new Buffer(attachment.content, attachment.encoding);
+              data = Buffer.from(attachment.content, attachment.encoding)
             } else {
-              data = attachment.content || attachment.path || undefined;
+              data = attachment.content || attachment.path || undefined
             }
 
             mailgunAttachment = new self.mailgun.Attachment({
@@ -139,38 +140,35 @@ MailgunTransport.prototype.send = function send(mail, callback) {
               filename: attachment.cid || attachment.filename || undefined,
               contentType: attachment.contentType || undefined,
               knownLength: attachment.knownLength || undefined
-            });
+            })
           }
 
           if (attachment.cid) {
-            inlineList.push(mailgunAttachment);
+            inlineList.push(mailgunAttachment)
           } else {
-            attachmentList.push(mailgunAttachment);
+            attachmentList.push(mailgunAttachment)
           }
         }
 
-        mailData.attachment = attachmentList;
-        mailData.inline = inlineList;
-        delete mailData.attachments;
+        mailData.attachment = attachmentList
+        mailData.inline = inlineList
+        delete mailData.attachments
       }
-
-      delete mail.data.headers;
-
-      var options = pickBy(mailData, function (value, key) {
+      delete mail.data.headers
+      const options = pickBy(mailData, function (value, key) {
         if (whitelistExact.indexOf(key) !== -1) {
-          return true;
+          return true
         }
-
         return some(whitelistPrefix, function (prefix) {
-          return startsWith(key, prefix);
-        });
-      });
+          return startsWith(key, prefix)
+        })
+      })
 
       self.messages.send(options, function (err, data) {
-        callback(err || null, data);
-      });
+        callback(err || null, data)
+      })
     }
   ], function (err) {
-    if (err) throw err;
-  });
-};
+    if (err) throw err
+  })
+}
